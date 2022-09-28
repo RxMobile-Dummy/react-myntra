6a4d5384c62161ab4d02b88106d103573c346878
Arjun <radixdt.2530@gmail.com>
2022-09-28 17:06:28 +0530

    Feat: Remove address api integration

---
+++ src/core/lib/adapters/reducer/removeAddressReducer.ts
@@ -0,0 +1,33 @@
+import { RemoveAddressActionType } from "../actionType/removeAddressActionType";
+import { RemoveAddressAction } from "../../useCases/removeAddressAction";
+
+
+type UserState = {
+  removeAdddata: any;
+  removeAddError: string | undefined;
+};
+
+const initialState = {
+  removeAdddata: undefined,
+  removeAddError: undefined,
+};
+
+const removeAddressReducer = (state: UserState = initialState, action: RemoveAddressAction): any => {
+  console.log("action payload:", action.payload);
+  switch (action.type) {
+    case RemoveAddressActionType.REMOVE_ADDRESS_SUCCESS:
+      return {
+        ...state,
+        removeAdddata: action.payload,
+      };
+    case RemoveAddressActionType.REMOVE_ADDRESS_FAILED:
+        return {
+          ...state,
+          removeAddError: action.payload,
+        };
+    default:
+      return state;
+  }
+};
+
+export default removeAddressReducer;
