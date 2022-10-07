8ba55276d60c2b1ed2e7521345bd13af44c4f9ff
radixdt2543 <radixdt.2543@gmail.com>
2022-10-07 11:46:20 +0530

    Feat: Forget password screen design

--- src/native/App.tsx
+++ src/native/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import ForgetPassword from './src/screens/ForgetPassword';
 import Login from './src/screens/Login';
 
 const App = () => {
